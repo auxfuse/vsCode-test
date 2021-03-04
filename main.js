f6d7d8fb7c8e65303431458eb0691eb045839dfc
Anthony O' Brien <aux.fuse@yahoo.com>
2021-03-04 23:08:53 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("gold_location <-- is the way to find the Gold!");
+
+function gold_location() {
+  let player = prompt("Who do they call thee?");
+  return `Hi, ${player}, the gold is https://... at the end of the rainbow, [img.rasset.ie/0010fb76-1600.jpg]`;
+};
